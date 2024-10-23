676911b7808a79e0029bc579b3e6f6f4d855ea14
BasilBasilio <cristiano.aru@gmail.com>
2024-10-23 16:20:08 +0200

    project inizialization

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,9 @@
+const Home: React.FC = () => {
+  return (
+    <>
+    <h1 className="bg-red-500">Test</h1>
+    </>
+  )
+}
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/*.{html,js,tsx}",
+    "./src/**/*.{html,js}",
+    "./src/components/**/*.{html,js,tsx}",
+    "./src/components/*.{html,js,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
