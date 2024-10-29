836067dafb2b9b6009d9a62cc0bd52aa00578539
BasilBasilio <cristiano.aru@gmail.com>
2024-10-29 13:54:56 +0100

    added navbar and layout on router tree

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,81 @@
+import {
+  FaUser,
+  FaHome,
+  FaCashRegister,
+  FaSignOutAlt,
+  FaSignInAlt,
+} from 'react-icons/fa';
+import { useUserAuth } from '../../context/userAuthContext';
+import { Link } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../../firebaseConfig';
+
+const bgColor = 'bg-blue-600';
+
+const Navbar: React.FC = () => {
+  const user = useUserAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate('/');
+    } catch (error) {
+      console.log('Error during logout', error);
+    }
+  };
+
+  return (
+    <nav className={`${bgColor} py-2 px-22`}>
+      <div className="flex justify-between mx-auto items-center py-4 px-24">
+        <div className="text-white font-bold text-xl">Gearfinder</div>
+        <ul className="flex gap-8 ml-auto mr-6 text-white cursor-pointer">
+          <li>
+            <Link to="/" className="flex items-center gap-2">
+              Home
+              <FaHome />
+            </Link>
+          </li>
+          {user ? (
+            <>
+              <li>
+                <button
+                  type="button"
+                  onClick={handleLogout}
+                  className="nav-link flex items-center gap-2"
+                >
+                  Logout
+                  <FaSignOutAlt />
+                </button>
+              </li>
+              <li>
+                <Link to="/user" className="flex items-center gap-2">
+                  <FaUser />
+                  <span>{user.email}</span>
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/register" className="flex items-center gap-2">
+                  Register
+                  <FaCashRegister />
+                </Link>
+              </li>
+              <li>
+                <Link to="/login" className="flex items-center gap-2">
+                  Login
+                  <FaSignInAlt />
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,26 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Layout from '../components/Layout/index.tsx';
 import Home from '../pages/home/index.tsx';
 import Error from '../pages/error/index.tsx';
 import Login from '../pages/login/index.tsx';
 import UserProfile from '../pages/user/index.tsx';
 import ProtectedRoutes from '../components/ProtectedRoutes/index.tsx';
 import UserSignUp from '../components/UserSignIn/index.tsx';
+
 export const router = createBrowserRouter([
-  {
-    element: <ProtectedRoutes />,
-    children: [
-      {
-        path: '/user',
-        element: <UserProfile />,
-        errorElement: <Error />,
-      },
-    ],
-  },
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <Error />,
-  },
   {
     path: '/register',
     element: <UserSignUp />,
@@ -32,9 +19,28 @@ export const router = createBrowserRouter([
     errorElement: <Error />,
   },
   {
-    path: '/*',
-    element: <Error />,
-    errorElement: <Error />,
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        element: <ProtectedRoutes />,
+        children: [
+          {
+            path: '/user',
+            element: <UserProfile />,
+            errorElement: <Error />,
+          },
+        ],
+      },
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/*',
+        element: <Error />,
+      },
+    ],
   },
 ]);
 
