2aae765ab45473a21d1fca9e6c680ca2b72dbcec
BasilBasilio <cristiano.aru@gmail.com>
2024-10-30 11:47:41 +0100

    deploy fix

--- src/components/UserSignIn/index.tsx
+++ src/components/UserSignIn/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { UserSignIn } from '../interfaces';
+import { UserSignIn } from './types';
 import { googleSignIn, signUp } from '../../auth';
 
 const initialValue: UserSignIn = {
