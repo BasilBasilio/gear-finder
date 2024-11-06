b891fb74694ff6e3909fcb278e6fd25d36860037
BasilBasilio <cristiano.aru@gmail.com>
2024-11-06 15:42:33 +0100

    added tests in login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('Login test redirect to homepage', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/login');
   await page.getByLabel('Email').fill('test@gmail.com');
   await page.getByLabel('Password').fill('testtest');
-  await page.getByRole('button', { name: 'Log in' }).click();
-  await page.waitForURL('/');
-  expect(page.url()).toBe('http://localhost:5173/');
+});
+
+test.describe('Login', () => {
+  test('should redirect to homepage', async ({ page }) => {
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.waitForURL('/');
+    expect(page.url()).toBe('http://localhost:5173/');
+  });
+
+  test('should access to user protected route', async ({ page }) => {
+    await page.goto('/user');
+    expect(page.url()).toBe('http://localhost:5173/user');
+  });
 });
