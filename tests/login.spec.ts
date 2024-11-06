2e7fe2c10fc1b3614c2865276cda556e14ad593a
BasilBasilio <cristiano.aru@gmail.com>
2024-11-06 12:46:10 +0100

    base url test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('Login test redirect to homepage', async ({ page }) => {
-  await page.goto('http://localhost:5173/login');
+  await page.goto('/login');
   await page.getByLabel('Email').fill('test@gmail.com');
   await page.getByLabel('Password').fill('testtest');
   await page.getByRole('button', { name: 'Log in' }).click();
-  await page.waitForURL('http://localhost:5173/');
-  expect(page.url()).toBe('http://localhost:5173/');
+  await page.waitForURL('/');
+  expect(page.url()).toBe('/');
 });
