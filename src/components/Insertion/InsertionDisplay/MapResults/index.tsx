a9b5ceeb992650821927ae5de48243a91fe37564
BasilBasilio <cristiano.aru@gmail.com>
2024-11-13 12:08:15 +0100

    fixes after review

---
+++ src/components/Insertion/InsertionDisplay/MapResults/index.tsx
@@ -0,0 +1,23 @@
+import { InsertionData } from '../../types';
+
+const MapResults: React.FC<InsertionData> = ({
+  id,
+  model,
+  instrumentType,
+  description,
+  rentalPrice,
+}) => {
+  return (
+    <li className="bg-white shadow-md rounded-lg p-4 transition-transform transform hover:scale-105">
+      <div>
+        <h2 className="text-lg font-semibold text-gray-900">{model}</h2>
+      </div>
+      <p className="text-gray-700 text-sm">{instrumentType}</p>
+      <p className="text-blue-500 font-bold"></p>
+      {description}
+      <p className="text-blue-500 font-bold">€{rentalPrice}/day</p>
+    </li>
+  );
+};
+
+export default MapResults;
--- src/components/Insertion/InsertionDisplay/index.tsx
+++ src/components/Insertion/InsertionDisplay/index.tsx
@@ -3,6 +3,7 @@ import { db } from '../../../firebaseConfig';
 import { useEffect, useState } from 'react';
 import { InsertionData } from '../types';
 import { useUserAuth } from '../../../context/userAuthContext';
+import MapResults from './MapResults';
 
 const InsertionDisplay: React.FC = () => {
   const [insertions, setInsertions] = useState<InsertionData[]>([]);
@@ -32,7 +33,6 @@ const InsertionDisplay: React.FC = () => {
 
   useEffect(() => {
     getData();
-    console.log(user);
   }, []);
 
   return (
@@ -42,24 +42,9 @@ const InsertionDisplay: React.FC = () => {
       </h2>
 
       <ul className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {insertions.map(insertion => {
-          const { id, instrumentType, model, description, rentalPrice } =
-            insertion;
-          return (
-            <li
-              key={id}
-              className="bg-white shadow-md rounded-lg p-4 transition-transform transform hover:scale-105"
-            >
-              <div>
-                <h2 className="text-lg font-semibold text-gray-900">{model}</h2>
-              </div>
-              <p className="text-gray-700 text-sm">{instrumentType}</p>
-              <p className="text-blue-500 font-bold"></p>
-              {description}
-              <p className="text-blue-500 font-bold">€{rentalPrice}/day</p>
-            </li>
-          );
-        })}
+        {insertions.map(insertion => (
+          <MapResults key={insertion.id} {...insertion} />
+        ))}
       </ul>
     </div>
   );
