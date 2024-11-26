6d6d09d6ea1d2ffedd622712bd24951651429f1e
BasilBasilio <cristiano.aru@gmail.com>
2024-11-26 16:49:43 +0100

    test fix

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,7 +9,7 @@ test('should access user protected route when logged in', async ({ page }) => {
   await page.goto('/login');
   await page.getByLabel('Email').fill('pay@gmail.com');
   await page.getByLabel('Password').fill('testtest');
-  await page.getByRole('button', { name: 'Log In' }).click();
+  await page.getByTestId('login').click();
   await page.waitForURL('/');
   await page.goto('/user');
   await expect(page).toHaveURL('/user');
